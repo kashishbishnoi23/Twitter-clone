9bb66036e08e5a6b663ecda279defea41b71a0d7
kashish <kashishbishnoi23@gmail.com>
2024-09-08 22:20:18 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors:{
+        grey:'rgb(100,104,109)',
+        // blue:'rgb(22,117,181)'
+        blue:'#1D9BF0',
+        search:'rgb(32,34,38)',
+        searchtext: '#5f6368',
+        lightgrey: 'rgb(0,1,1)',
+        icon_bg: '#1D9BF01A',
+        hover_bg: 'rgb(255, 255, 255, 0.03)'
+
+    
+      }
+    },
+  },
+  plugins: [],
+}
+
